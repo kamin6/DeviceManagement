55196d507558f3f24e09c32d0d15d7e14efa8b30
Khushi Amin <58108801+kamin6@users.noreply.github.com>
2023-03-28 19:58:35 +0000

    added the html pages

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-  return "<p>Hello World</p>"
+def home():
+  return render_template('home.html')
 
+@app.route("/devices")
+def devices():
+  return render_template('devices.html')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
