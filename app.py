b1aecc55f9b9928c58a43b24e5b405045d7e3845
Khushi Amin <58108801+kamin6@users.noreply.github.com>
2023-04-18 11:13:45 +0000

    Retrying fix

--- app.py
+++ app.py
@@ -71,5 +71,22 @@ def get_all_devices():
     list_of_tables.append(load_devices_from_db(member))
   return list_of_tables
 
+# Method for fetching all the members from the database
+def load_members_from_db():
+  with engine.connect() as conn:
+    members_data = conn.execute(text("select * from members"))
+    members = {}
+    for row in members_data.all():
+      members[row[0]] = row[1]
+    return members
+
+# Method for fetching all the devices associated with the given memberId
+def load_devices_from_db(memberId):
+  with engine.connect() as conn:
+    devices_data = conn.execute(
+      text(f"""select * from devices where OWNER='{memberId}'"""))
+    devicesOfMember = list(devices_data)
+    return devicesOfMember
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -11,21 +11,4 @@ engine = create_engine(
     }
   }
 )
-
-# Method for fetching all the members from the database
-def load_members_from_db():
-  with engine.connect() as conn:
-    members_data = conn.execute(text("select * from members"))
-    members = {}
-    for row in members_data.all():
-      members[row[0]] = row[1]
-    return members
-
-# Method for fetching all the devices associated with the given memberId
-def load_devices_from_db(memberId):
-  with engine.connect() as conn:
-    devices_data = conn.execute(
-      text(f"""select * from devices where OWNER='{memberId}'"""))
-    devicesOfMember = list(devices_data)
-    return devicesOfMember
     
